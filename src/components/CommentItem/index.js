d0fb931f9e807556641cb83fd775633ded2511f6
Naresh141427 <a.nareshindian@gmail.com>
2023-02-15 07:56:07 +0000

    omments app

--- src/components/CommentItem/index.js
+++ src/components/CommentItem/index.js
@@ -1,1 +1,63 @@
-// Write your code here
+import './index.css'
+
+const CommentItem = props => {
+  const {commentsDetails, classNameList, onLiking, deleteComment} = props
+  const {
+    id,
+    inputText,
+    comments,
+    time,
+    commentsCount,
+    isLiked,
+  } = commentsDetails
+
+  const onDoingLike = () => {
+    onLiking(id)
+  }
+
+  const onDeleting = () => {
+    deleteComment(id)
+  }
+
+  const imageUrl = isLiked
+    ? 'https://assets.ccbp.in/frontend/react-js/comments-app/liked-img.png'
+    : 'https://assets.ccbp.in/frontend/react-js/comments-app/like-img.png'
+
+  return (
+    <li className="comment-item">
+      <div className="top-container">
+        <div className={`name-container ${classNameList[commentsCount - 1]}`}>
+          {inputText[0]}
+        </div>
+        <div className="name-time-container">
+          <div className="container">
+            <p className="name">{inputText}</p>
+            <p className="time">{time}</p>
+          </div>
+          <p className="description">{comments}</p>
+        </div>
+      </div>
+      <div className="bottom-container">
+        <button type="button" className="like-button">
+          <img
+            src={imageUrl}
+            className="like-image"
+            alt="like"
+            onClick={onDoingLike}
+          />
+        </button>
+        <button type="button" className="like-button" data-testid="delete">
+          <img
+            src="https://assets.ccbp.in/frontend/react-js/comments-app/delete-img.png"
+            className="like-image"
+            alt="delete"
+            onClick={onDeleting}
+          />
+        </button>
+      </div>
+      <hr className="hr-rule" />
+    </li>
+  )
+}
+
+export default CommentItem
--- src/components/Comments/index.js
+++ src/components/Comments/index.js
@@ -1,5 +1,9 @@
 import {Component} from 'react'
 import './index.css'
+import {v4 as uuidv4} from 'uuid'
+import {formatDistanceToNow} from 'date-fns'
+
+import CommentItem from '../CommentItem'
 
 const initialContainerBackgroundClassNames = [
   'amber',
@@ -12,20 +16,132 @@ const initialContainerBackgroundClassNames = [
 ]
 
 class Comments extends Component {
+  state = {
+    commentsList: [],
+    inputText: '',
+    comments: '',
+    commentsCount: 0,
+    time: '',
+    isLiked: false,
+  }
+
+  onLiking = id => {
+    this.setState(prevState => ({
+      commentsList: prevState.commentsList.map(each => {
+        if (each.id === id) {
+          return {...each, isLiked: !each.isLiked}
+        }
+        return each
+      }),
+    }))
+  }
+
+  deleteComment = id => {
+    this.setState(prevState => ({
+      commentsList: prevState.commentsList.filter(each => {
+        if (each.id !== id) {
+          return each
+        }
+        return ''
+      }),
+    }))
+  }
+
   onAddingComments = event => {
     event.preventDefault()
+    const {
+      inputText,
+      comments,
+      commentsList,
+      isLiked,
+      commentsCount,
+    } = this.state
+    const count = commentsList.length + 1
+    const newComment = {
+      id: uuidv4(),
+      inputText,
+      comments,
+      commentsCount: count,
+      time: formatDistanceToNow(new Date()),
+      isLiked,
+    }
+    this.setState(prevState => ({
+      commentsList: [...prevState.commentsList, newComment],
+      inputText: '',
+      comments: '',
+      time: '',
+      commentsCount,
+      isLiked,
+    }))
+  }
+
+  onEnteringText = event => {
+    this.setState({inputText: event.target.value})
+  }
+
+  onEnteringComments = event => {
+    this.setState({comments: event.target.value})
   }
 
   render() {
+    const {commentsList, inputText, comments, commentsCount} = this.state
     return (
       <div className="app-container">
-        <h1 className="header">Comments</h1>
-        <p className="description">Say something about 4.0 Technologies</p>
-        <form className="comments-form" onSubmit={this.onAddingComments}>
-          <input type="text" className="input-bar" placeholder="your name" />
-          <br />
-          <textarea className="input-bar" rows="8" cols="80"></textarea>
-        </form>
+        <div className="top-section">
+          <div className="input-container">
+            <h1 className="header">Comments</h1>
+            <p className="description">Say something about 4.0 Technologies</p>
+            <form className="comments-form" onSubmit={this.onAddingComments}>
+              <input
+                type="text"
+                className="input-bar"
+                placeholder="Your Name"
+                onChange={this.onEnteringText}
+                value={inputText}
+              />
+              <br />
+              <textarea
+                className="input-bar"
+                rows="8"
+                cols="80"
+                placeholder="Your Comment"
+                onChange={this.onEnteringComments}
+                value={comments}
+              />
+              <button type="submit" className="btn">
+                Add Comment
+              </button>
+            </form>
+          </div>
+          <img
+            src="https://assets.ccbp.in/frontend/react-js/comments-app/comments-img.png"
+            alt="comments"
+            className="comments-image"
+          />
+        </div>
+        <hr className="horizontal-rule" />
+        <div className="bottom-section">
+          <div className="comment-container">
+            <button type="button" className="add-button">
+              {commentsCount}
+            </button>
+            <p className="comments-header">Comments</p>
+          </div>
+        </div>
+
+        <ul className="comments-list-container">
+          {commentsList
+            ? commentsList.map(each => (
+                <CommentItem
+                  commentsDetails={each}
+                  key={each.id}
+                  classNameList={initialContainerBackgroundClassNames}
+                  onLiking={this.onLiking}
+                  deleteComment={this.deleteComment}
+                />
+              ))
+            : ''}
+        </ul>
       </div>
     )
   }
